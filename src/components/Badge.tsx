8b24b767d2fc5d43bf15d7b00316375cf87c5bd5
Santanu Dasgupta <santanu@Santanus-MacBook-Air.local>
2023-09-18 10:23:57 +0530

    Brand

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -61,7 +61,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> GoCoCo</span>
       </a>
     </span>
   )
